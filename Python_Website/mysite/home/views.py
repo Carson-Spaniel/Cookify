dd1dfd9199aa68dac4cd221d876527a4ad12dfd7
Carson Spaniel <carsonspaniel@gmail.com>
2023-06-12 12:22:52 -0500

    update v1.00

--- Python_Website/mysite/home/views.py
+++ Python_Website/mysite/home/views.py
@@ -33,5 +33,8 @@ def library(request):
 def login(request):
     return render(request, "registration/login.html",)
 
+def signup(request):
+    return render(request, "registration/signup.html",)
+
 def home(request):
     return render(request, "main/home.html",)
