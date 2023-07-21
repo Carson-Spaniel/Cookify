fca0f4b997e076c1a4a987cf147b12c5e7012f1b
Carson-Spaniel <carsonspaniel@gmail.com>
2023-07-21 17:59:09 -0500

    deploy

--- recipesite/settings.py
+++ recipesite/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&$6rna)-gu36duh@u9hp#$p)0kjg@2$(_4*+a(rx57)eik2$-e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ['HEROKU_APP_DOMAIN']]
 
 # Application definition
 
