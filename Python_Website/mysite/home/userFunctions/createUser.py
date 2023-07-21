9def353be5ad69576db9ead951636193e99cdd15
Carson-Spaniel <carsonspaniel@gmail.com>
2023-07-21 16:01:29 -0500

    Fix buildpack and clear cache

--- Python_Website/mysite/home/userFunctions/createUser.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import User
-user = User.objects.create_user("carson", "carsonspaniel@gmail.com", "sand123")
-
-# At this point, user is a User object that has already been saved
-# to the database. You can continue to change its attributes
-# if you want to change other fields.
-user.last_name = "Spaniel"
-user.save()
