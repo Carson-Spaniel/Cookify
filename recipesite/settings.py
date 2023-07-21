57d8b76f8bf6532ee2ec2f8f1d95e1637a348014
Carson-Spaniel <carsonspaniel@gmail.com>
2023-07-21 16:09:35 -0500

    Fix static root

--- recipesite/settings.py
+++ recipesite/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+from .secrets import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,10 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&$6rna)-gu36duh@u9hp#$p)0kjg@2$(_4*+a(rx57)eik2$-e'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -104,6 +105,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'C:/Users/carso/recipesite/staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
