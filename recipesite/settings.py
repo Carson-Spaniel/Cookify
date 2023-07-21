0d03039302fe0128a647f6626d8671b729e57291
Carson-Spaniel <carsonspaniel@gmail.com>
2023-07-21 18:40:09 -0500

    Add whitenoise to INSTALLED_APPS

--- recipesite/settings.py
+++ recipesite/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'recipeApp.apps.RecipeappConfig',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
