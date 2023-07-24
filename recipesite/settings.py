91092f410103a6f1f7e420f5249bf00b75421e78
Carson-Spaniel <carsonspaniel@gmail.com>
2023-07-24 00:06:51 -0500

    had debug True this whole time

--- recipesite/settings.py
+++ recipesite/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['cookify-16e5ae3525c7.herokuapp.com', 'cookify-recipes.com', 'www.cookify-recipes.com', '127.0.0.1']
 
