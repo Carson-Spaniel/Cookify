e35ebf18a4573f42975059884f281dc57f4c82b0
Carson-Spaniel <carsonspaniel@gmail.com>
2023-07-25 22:20:40 -0500

    fix

--- recipeApp/functions/checkLogin.py
+++ recipeApp/functions/checkLogin.py
@@ -5,7 +5,7 @@ import json
 if DEBUG:
     from recipesite.secrets import USERS_ENV_VAR as users
 else:
-    users_json = os.environ.get("USERS_ENV_VAR", "{}")
+    users_json = os.environ.get("USERS_ENV_VAR", "{}") #users_env_var must be in double quotes
     users = json.loads(users_json)
 
 def checkLoginFun(username, password):
