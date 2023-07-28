60d11a13ad30d69f7ba5861bea3be017dafd2132
Carson Spaniel <carsonspaniel@gmail.com>
2023-07-28 12:53:48 -0500

    clean up views.py

--- recipeApp/views.py
+++ recipeApp/views.py
@@ -71,8 +71,6 @@ def searchRecipe(request):
     if request.method == 'POST':
         input_data = request.POST.get('input', '')
         recipeArray = searchRecipes(input_data)
-        #recipeArray = [[628303, 'Easy Chicken Alfredo Pasta Bake', 'https://spoonacular.com/recipeImages/628303-312x231.jpg'], [1132972, 'Easy Chicken Alfredo Pasta Bake', 'https://spoonacular.com/recipeImages/1132972-312x231.jpg'], [504219, 'Easy Chicken Alfredo Pasta Bake with Sun-Dried Tomatoes', 'https://spoonacular.com/recipeImages/504219-312x231.jpg'], [617370, 'Cheesy Chicken Alfredo Pasta Bake', 'https://spoonacular.com/recipeImages/617370-312x231.jpg'], [1587471, 'One Pot Chicken Alfredo Pasta', 'https://spoonacular.com/recipeImages/1587471-312x231.jpg'], [1040819, 'Instant Pot Chicken Alfredo Pasta (pressure cooker pasta)', 'https://spoonacular.com/recipeImages/1040819-312x231.jpg'], [1037262, 'One Pot Taco Chicken Alfredo Pasta', 'https://spoonacular.com/recipeImages/1037262-312x231.jpg'], [925369, 'Simple One-Skillet Chicken Alfredo Pasta', 'https://spoonacular.com/recipeImages/925369-312x231.jpg'], [139368, 'Chicken Alfredo Pesto Pasta', 'https://spoonacular.com/recipeImages/139368-312x231.jpg'], [617946, 'Chicken Pesto Alfredo Pasta', 'https://spoonacular.com/recipeImages/617946-312x231.jpg'], [267282, 'Chicken Alfredo Pesto Pasta', 'https://spoonacular.com/recipeImages/267282-312x231.jpg'], [714728, '15 Minute Chicken Bacon Alfredo Pasta', 'https://spoonacular.com/recipeImages/714728-312x231.jpg'], [619396, 'Pumpkin Chicken Alfredo & Bacon Pasta', 'https://spoonacular.com/recipeImages/619396-312x231.jpg'], [138989, 'Chicken and Pasta Alfredo - Weight Watchers', 'https://spoonacular.com/recipeImages/138989-312x231.jpg'], [582582, 'Slow Cooker Garlic Chicken and Veggie Alfredo Pasta', 'https://spoonacular.com/recipeImages/582582-312x231.jpg'], [563082, 'Gluten Free Pasta : Chicken Alfredo', 'https://spoonacular.com/recipeImages/563082-312x231.jpg'], [594476, 'Alfredo Pasta with Cajun Spiced Chicken', 'https://spoonacular.com/recipeImages/594476-312x231.jpg'], [138937, 'Blackened Chicken Pasta With Alfredo Sauce', 'https://spoonacular.com/recipeImages/138937-312x231.jpg'], [779603, 'Creamy Pesto Alfredo Chicken Pasta', 'https://spoonacular.com/recipeImages/779603-312x231.jpg']]
-        # text = []
         response_data = {'text': recipeArray}
         print("\nExiting post")
         return JsonResponse(response_data)
@@ -97,30 +95,6 @@ def loadRecipe(request):
     else:
         print("Exiting post with error")
         return JsonResponse({'error': 'Invalid request method'}, status=400)
-    
-# @csrf_exempt
-# def checkLogin(request):
-#     print("Entering post\n")
-#     if request.method == 'POST':
-#         username = request.POST.get('username', '')
-#         password = request.POST.get('password', '')
-#         usernameCheck, passwordCheck = checkLoginFun(username,password)
-#         if usernameCheck and passwordCheck:
-#             print("Should login")
-#             return render(request, 'webpages/index.html')
-#         elif usernameCheck and not passwordCheck:
-#             response_data = {
-#                 'text': 'wrongPass'
-#                 }
-#         else:
-#             response_data = {
-#                 'text': 'wrongUser'
-#                 }
-#         print("\nExiting post")
-#         return JsonResponse(response_data)
-#     else:
-#         print("Exiting post with error")
-#         return JsonResponse({'error': 'Invalid request method'}, status=400)
     
 # ------------- breakfast pages -------------
 def breakfastCasserole(request):
